e1ce988ae527aa30d1fd61155467b625d43b65f6
Dien DUONG <hi@duongludien.com>
2022-07-30 19:57:38 +0700

    feat: add stopwatch to calculate the processing time

--- csharp/SortingAlgorithms/Program.cs
+++ csharp/SortingAlgorithms/Program.cs
@@ -1,10 +1,39 @@
-﻿using SortingAlgorithms;
+﻿using System.Diagnostics;
+using SortingAlgorithms;
 
-var array = new int[] {5, 8, 2, 10, 5, 12, 8, 1, 15, 4};
+// var array = new int[] {5, 8, 2, 10, 5, 12, 8, 1, 15, 4};
+//
+// QuickSort.SortAscending(array, 0, array.Length - 1);
+//
+// foreach (var item in array)
+// {
+//     Console.WriteLine(item);
+// }
 
-QuickSort.SortAscending(array, 0, array.Length - 1);
+var random = new Random();
+var array = Enumerable.Range(0, 1000)
+    .Select(_ => random.Next(0, 1000))
+    .ToArray();
 
-foreach (var item in array)
+var array1 = new int[array.Length];
+var array2 = new int[array.Length];
+array.CopyTo(array1, 0);
+array.CopyTo(array2, 0);
+
+var stopwatch1 = Stopwatch.StartNew();
+QuickSort.SortAscending(array1, 0, array1.Length - 1);
+stopwatch1.Stop();
+Console.WriteLine($"Total time (Quick sort): {stopwatch1.ElapsedTicks}");
+
+var stopwatch2 = Stopwatch.StartNew();
+Array.Sort(array2);
+stopwatch2.Stop();
+Console.WriteLine($"Total time (.NET Array.Sort): {stopwatch2.ElapsedTicks}");
+
+var correct = true;
+for (var i = 0; i < array1.Length; ++i)
 {
-    Console.WriteLine(item);
+    if (array1[i] != array2[i]) correct = false;
 }
+
+Console.WriteLine(correct);
